6fd2eeea6d9cb1a01112eef3e158cc93a6486a63
Alex <lipatnikov@yandex.ru>
2022-04-20 23:29:16 +0500

    Add Example012_Methods

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,31 @@
+﻿// вид метода 1
+
+/*
+void Method1()
+{
+    Console.WriteLine("Автор Липатников Александр");
+}
+
+Method1();
+*/
+
+// вид метода 2
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+//Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i=0;
+    while (i<count)
+    {
+        Console.WriteLine(msg);
+        i++;
+
+    }
+}
+Method21("Текст", 4);
