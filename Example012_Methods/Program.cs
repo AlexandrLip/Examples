ac1037c9d5cc34fb7251af74bb6ff8e97fc3b58d
Alex <lipatnikov@yandex.ru>
2022-04-20 23:39:17 +0500

    Add Example012_Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -11,6 +11,7 @@ Method1();
 
 // вид метода 2
 
+/*
 void Method2(string msg)
 {
     Console.WriteLine(msg);
@@ -28,4 +29,36 @@ void Method21(string msg, int count)
 
     }
 }
-Method21("Текст", 4);
+Method21(msg: "Текст", count: 4);
+
+*/
+
+// вид метода 3
+
+/*
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+*/
+
+// вид метода 4
+
+string Method4(int count, string Text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + Text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "клеим текст");
+Console.WriteLine(res);
